9732aa9988af2e6815028096a96598a243cdb4c9
Makhach I Israpilov <lovnager@gmail.com>
2016-09-30 22:48:29 +0300

    Update blot.pl

--- task7/blot.pl
+++ task7/blot.pl
@@ -1,11 +1,10 @@
-lessons(_):-true.
+not_lessons(_):-true.
 a(X):-not(X=vitya), X=borya.
 b(X):-X=vitya, not(X=alesha).
-v(X):-not(X=borya), lessons(vitya).
+v(X):-not(X=borya), not_lessons(vitya).
 blot(X):- a(X), b(X), not(v(X)).
 blot(X):- a(X), not(b(X)), v(X).
 blot(X):- not(a(X)), b(X), v(X).
-
 
 
 
