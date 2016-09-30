70e182679fe5e3c9f7b0a5273a75b6a44328c51c
Makhach I Israpilov <lovnager@gmail.com>
2016-09-30 22:44:10 +0300

    Create blot.pl

--- task7/blot.pl
+++ task7/blot.pl
@@ -1,7 +1,11 @@
-lessons(X):-true.
+lessons(_):-true.
 a(X):-not(X=vitya), X=borya.
 b(X):-X=vitya, not(X=alesha).
 v(X):-not(X=borya), lessons(vitya).
 blot(X):- a(X), b(X), not(v(X)).
 blot(X):- a(X), not(b(X)), v(X).
 blot(X):- not(a(X)), b(X), v(X).
+
+
+
+
