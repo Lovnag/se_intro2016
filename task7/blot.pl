f70812b2eae143beb420e30c0b6abdb0b7812083
Makhach I Israpilov <lovnager@gmail.com>
2016-10-01 00:16:53 +0300

    Update blot.pl

--- task7/blot.pl
+++ task7/blot.pl
@@ -1,10 +1,10 @@
-lessons(K):-hell.
+not_lessons(Y):-true.
 a1(X):-not(X=vitya).
 a2(X):- X=borya.
 b1(X):-X=vitya.
 b2(X):-not(X=alesha).
 v1(X):-not(X=borya).
-v2:- not(lessons(vitya)).
-blot(X):- a1(X), a2(X), b1(X), b2(X), (not(v1(X)). 	
-blot(X):- a1(X), a2(X), (not(b1(X)); not(b2(X))), v1(X).
-blot(X):- (not(a1(X));not(a2(X))), b1(X), b2(X), v1(X).
+v2(Y):- not_lessons(vitya).
+blot(X):- a1(X), a2(X), b1(X), b2(X), (not(v1(X); not(v2(Y))). 	
+blot(X):- a1(X), a2(X), (not(b1(X)); not(b2(X))), v1(X), v2(Y).
+blot(X):- (not(a1(X));not(a2(X))), b1(X), b2(X), v1(X), v2(Y).
