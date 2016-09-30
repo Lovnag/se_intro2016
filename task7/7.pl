558d0b035cf818650ace334eeef7f247325addf3
Makhach I Israpilov <lovnager@gmail.com>
2016-09-30 19:31:40 +0300

    Update 7.pl

--- task7/7.pl
+++ task7/7.pl
@@ -1,6 +1,7 @@
 a(X):-not(X=vitya), X=borya.
 b(X):-X=vitya, not(X=alesha).
-v(X):-not(X=borya).
-blot(X):-a(X), b(X), not(v(X)).
-blot(X):-a(X), not(b(X)),  v(X).
-blot(X):-not(a(X)), b(X), v(X).
+v(X, Y):-not(X=borya), Y=vitya.
+blot(X):-a(X), b(X), not(v(X,Y)).
+blot(X):-a(X), not(b(X)),  v(X,Y).
+blot(X):-not(a(X)), b(X), v(X,Y).
+lessons(Y):-v(X,Y).
