691a8055ac01693a730aca60f0ae388fc0169c58
Makhach I Israpilov <lovnager@gmail.com>
2016-11-10 02:51:56 +0300

    Create Main.java

---
+++ task17/Main.java
@@ -0,0 +1,27 @@
+public class Main{
+	public static void main(String[] args)
+	{
+		System.out.println(polynome("basb"));
+	}
+	
+	
+	public static boolean polynome(String s)
+	{
+		if ( (s.length()==0) || (s.length()==1) ) 
+		{
+			return true;
+		}
+		else if ( s.charAt(0)!=s.charAt(s.length()-1)) 
+		{
+			return false;
+		}
+		else if (s.length()==2)
+        {
+          return true;
+        }
+      else
+		{
+			return(polynome(s.substring(1,s.length()-2)));
+		}
+	}
+}
