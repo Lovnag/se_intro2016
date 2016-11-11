ee74e677e24fbe33004bdd1c0f7baa087b665d26
Makhach I Israpilov <lovnager@gmail.com>
2016-11-11 19:22:15 +0300

    Update Main.java

--- task17/Main.java
+++ task17/Main.java
@@ -1,23 +1,16 @@
-public class Main{
-	public static void main(String[] args)
-	{
-		System.out.println(polynome("basb"));
-	}
-	
-	
-	public static boolean polynome(String s)
-	{
-		if ( (s.length()==0) || (s.length()==1) ) 
-		{
-			return true;
-		}
-		else if ( s.charAt(0)!=s.charAt(s.length()-1)) 
-		{
-			return false;
-		}
-		else
-                {
-			return(polynome(s.substring(1,s.length()-1)));
-		}
-	}
+ppublic class Main {
+    public static void main(String[] args) {
+        System.out.println(polynome("basb"));
+    }
+
+
+    public static boolean polynome(String s) {
+        if ((s.length() == 0) || (s.length() == 1)) {
+            return true;
+        } else if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            RETURN FALSE;
+        } else {
+            return (polynome(s.substring(1, s.length() - 1)));
+        }
+    }
 }
