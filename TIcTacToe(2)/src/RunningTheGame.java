00a9118ee6e2c608872505a952da85cd61e24f80
Makhach I Israpilov <lovnager@gmail.com>
2016-11-20 23:24:32 +0300

    Add files via upload

---
+++ TIcTacToe(2)/src/RunningTheGame.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+/**
+ * Created by thy humble Lovnag on 20.11.2016.
+ */
+public class RunningTheGame {
+
+    public RunningTheGame() {
+    }
+
+    public static String Run() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Greetings, dear GameMaster. Dear GameMaster, please enter the number of players:");
+        int numbOfPlayers = in.nextInt();
+        in.nextLine();
+        Player[] players = new Player[numbOfPlayers];
+        int[] allegiance = new int[numbOfPlayers];
+        char lable;
+        String playerName;
+        System.out.println("Very well. Now please, for every player enter (1), if it will be a real person, or (0), if it will be a highly advanced AI");
+        System.out.println("Then enter a character that will mark their turn, and lastly - the name under which they will be known on the battlefield:");
+        for (int i = 0; i < numbOfPlayers; i++) {
+            playerName = in.nextLine();
+            allegiance[i] = Character.getNumericValue(playerName.charAt(0));
+            lable = playerName.charAt(2);
+            playerName = playerName.substring(4);
+
+            if (allegiance[i] == 0) {
+                players[i] = new RandomGenPlayer(lable, playerName);
+            } else {
+                players[i] = new RealLifePlayer(lable, playerName);
+            }
+        }
+
+        System.out.println("Now enter the length and the width of the battlefield:");
+        int x = in.nextInt();
+        in.nextLine();
+        int y = in.nextInt();
+        in.nextLine();
+
+        System.out.println("And finally, what would be your win conditions?");
+        WinConditions win = new WinConditions(in.nextInt());
+        in.nextLine();
+        System.out.println("Now then, lets begin!");
+
+        TurnCoordinates CurrentCoordinates = new TurnCoordinates(0, 0);
+        int turnCounter = 0;
+        int CurrPlayerNumber;
+
+        char[][] duhField =
+                new char[x][y];
+
+        Field f = new Field(x, y, duhField);
+        f.initialize();
+        duhField[0][0] = '`';
+        Game newGame = new Game(x, y, players, f);
+        while (!win.Met(CurrentCoordinates, newGame.getDeck(), true)) {
+            System.out.println("Currently, the desk looks like this:");
+            System.out.println(newGame.getDeck());
+            CurrPlayerNumber = turnCounter % numbOfPlayers;
+            while (!newGame.getDeck().canTurn(CurrentCoordinates)) {
+                if (allegiance[CurrPlayerNumber] == 1) {
+                    CurrentCoordinates = players[CurrPlayerNumber].gettingATurn();
+                } else {
+                    CurrentCoordinates = players[CurrPlayerNumber].gettingATurn(x, y);
+                    System.out.println(CurrentCoordinates);
+                }
+
+                if (!newGame.getDeck().canTurn(CurrentCoordinates)) {
+                    System.out.println("Sorry, but this spot is already filled by someone. Try one more time:");
+                }
+            }
+
+            newGame.createATurn(CurrentCoordinates, players[CurrPlayerNumber]);
+            turnCounter++;
+        }
+
+        if (win.Met(CurrentCoordinates, newGame.getDeck(), false)) {
+
+
+            System.out.println("And now, ladies and gentlemen, we have a winner!");
+            turnCounter--;
+            System.out.println("Turn:" + turnCounter);
+            System.out.println("The desk:");
+            System.out.println(newGame.getDeck());
+            System.out.println("The winning player:");
+            System.out.println(players[turnCounter % numbOfPlayers].getPlayerName());
+
+            return "Congratulations to the " + players[turnCounter % numbOfPlayers].getPlayerName() + "!";
+        } else {
+            return "Unfortunately, it seems this battle ends with a draw";
+        }
+    }
+}
---
+++ TIcTacToe(2)/src/WinConditions.java
@@ -0,0 +1,111 @@
+/**
+ * Created by thy humble Lovnag on 19.11.2016.
+ */
+public class WinConditions {
+    private int numberToWin;
+
+    public WinConditions(int numberToWin) {
+        this.numberToWin = numberToWin;
+    }
+
+    public int getNumberToWin() {
+        return numberToWin;
+    }
+
+    public boolean Met(TurnCoordinates a, Field deck, boolean indicator) {
+        int x = a.getX();
+        int y = a.getY();
+        int sizX = deck.getSizeX();
+        int sizY = deck.getSizeY();
+        char[][] theDeck = deck.getDuhField();
+        char label = theDeck[x][y];
+        int kol = 0;
+        for (int i = 0; i < sizX; i++) {
+            if (theDeck[i][y] == label) {
+                kol++;
+            } else {
+                kol = 0;
+            }
+            if (kol == numberToWin) {
+                return true;
+            }
+        }
+
+        kol = 0;
+
+        for (int i = 0; i < sizY; i++) {
+            if (theDeck[x][i] == label) {
+                kol++;
+            } else {
+                kol = 0;
+            }
+            if (kol == numberToWin) {
+                return true;
+            }
+        }
+
+        int curX = x;
+        int curY = y;
+        while ((theDeck[curX][curY] == label) && (curX >= 0) && (curX < sizX - 1) && (curY >= 0) && (curY < sizY - 1)) {
+            curX++;
+            curY++;
+        }
+
+
+        if ((curX > 0) && (curY > 0)) {
+            curX--;
+            curY--;
+        }
+        kol = 0;
+        while (theDeck[curX][curY] == label) {
+            kol++;
+            if (kol == numberToWin) {
+                return true;
+            }
+            if ((curX > 0) && (curY > 0)) {
+                curX--;
+                curY--;
+            } else {
+                break;
+            }
+
+        }
+
+        curX = x;
+        curY = y;
+        while ((theDeck[curX][curY] == label) && (curX > 0) && (curX < sizX) && (curY >= 0) && (curY < sizY - 1)) {
+            curX--;
+            curY++;
+        }
+        if ((curX < sizX - 1) && (curY > 0)) {
+            curX++;
+            curY--;
+        }
+        kol = 0;
+        while (theDeck[curX][curY] == label) {
+            kol++;
+            if (kol == numberToWin) {
+                return true;
+            }
+            if ((curX < sizX-1) && (curY > 0)) {
+                curX++;
+                curY--;
+            } else {
+                break;
+            }
+        }
+
+        if (indicator) {
+        for (int i = 0; i < sizX; i++) {
+            for (int j = 0; j < sizY; j++) {
+                if ((theDeck[i][j] == ' ') || (theDeck[i][j] == '`')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+        } else {
+            return false;
+        }
+    }
+}
